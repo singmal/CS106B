8a394544846d671c1d07cd1288170dafb1b1c1f2
Sigmal <2955106812@qq.com>
2024-02-28 21:02:18 +0800

    Learning CS106B: Day 04-02, finished L8 and A2 Q2-4

--- assignments/Serafini/WordLadder/src/wordladder.cpp
+++ assignments/Serafini/WordLadder/src/wordladder.cpp
@@ -7,13 +7,20 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <algorithm>
 #include "console.h"
+#include "set.h"
+#include "stack.h"
+#include "queue.h"
 #include "lexicon.h"
 
 using namespace std;
 
 void WelcomeInfo();
 void LoadDict(Lexicon& dict);
+void InputWord(int n, string& word);
+void FindLadder(Lexicon& dict, string& word1, string& word2);
+void PrintWordsFound(Stack<string>& words);
 
 int main()
 {
@@ -21,6 +28,23 @@ int main()
 
     Lexicon dict;
     LoadDict(dict);
+
+    // 有什么避免两次复制粘贴的代码重用好办法？
+    string word_start, word_end;
+    InputWord(1, word_start);
+    if (word_start.length() == 0)
+    {
+        cout << "Have a nice day.";
+        return 0;
+    }
+    InputWord(2, word_end);
+    if (word_end.length() == 0)
+    {
+        cout << "Have a nice day.";
+        return 0;
+    }
+
+    FindLadder(dict, word_start, word_end);
 
     return 0;
 }
@@ -39,7 +63,7 @@ void LoadDict(Lexicon& dict)
     ifstream infile;
     cout << "Dictionary file name? ";
     string filename;
-    cin >> filename;
+    getline(cin, filename);
 
     infile.open(filename);
     string word;
@@ -48,5 +72,65 @@ void LoadDict(Lexicon& dict)
         dict.add(word);
     }
 
+    cout << endl;
+}
+
+void InputWord(int n, string& word)
+{
+    cout << "Word #" << to_string(n) << " (or Enter to quit): ";
+    getline(cin, word);
+    transform(word.begin(), word.end(), word.begin(), tolower);
+}
+
+void FindLadder(Lexicon& dict, string& word1, string& word2)
+{
+    Queue<Stack<string>> frontier;
+    Set<string> explored_set;
+    Stack<string> init;
+    init.push(word1);
+    explored_set.add(word1);
+    if (word1 == word2)
+    {
+        PrintWordsFound(init);
+        return;
+    }
+    frontier.enqueue(init);
+
+    while (!frontier.isEmpty())
+    {
+        Stack<string> ladder = frontier.dequeue();
+        string word = ladder.peek();
+        for (int i = 0; i < word.length(); i++)
+        {
+            char old_letter = word[i];
+            for (int j = 0; j < 24; j++)
+            {
+                char new_letter = 'a' + j;
+                word[i] = new_letter;
+
+                if (dict.contains(new_letter) && old_letter != new_letter && !explored_set.contains(word))
+                {
+                    ladder.push(word);
+                    frontier.enqueue(ladder);
+                    explored_set.add(word);
+
+                    if (word == word2)
+                    {
+                        PrintWordsFound(ladder);
+                        return;
+                    }
+                }
+            }
+            word[i] = old_letter;
+        }
+    }
+}
+
+void PrintWordsFound(Stack<string>& words)
+{
+    while (!words.isEmpty())
+    {
+        cout << words.pop() << " ";
+    }
     cout << endl;
 }
