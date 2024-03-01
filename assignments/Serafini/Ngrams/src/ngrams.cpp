ff1e52848a1dcbdd1f4c518a56a9d8f1cb330244
Sigmal <2955106812@qq.com>
2024-03-01 22:02:34 +0800

    Learning CS106B: Day 06, almost finished A2.2

--- assignments/Serafini/Ngrams/src/ngrams.cpp
+++ assignments/Serafini/Ngrams/src/ngrams.cpp
@@ -7,17 +7,21 @@
 #include <fstream>
 #include <iostream>
 #include <string>
-#include <vector>
+#include "vector.h"
 #include "console.h"
 #include "strlib.h"
 #include "map.h"
+#include "random.h"
 
 using namespace std;
 
 // 不断提示输入直到成功的两种写法
 void PromptForFile(ifstream& infile);
 bool PromptForN(int &num);
-void ConstructMap(ifstream& infile, Map<vector<string>, vector<string>> map);
+void ConstructMap(ifstream& infile, Map<Vector<string>, Vector<string> >& map, int N);
+void MapInsertKey(Map<Vector<string>, Vector<string> >& map, Vector<string>& window, string word);
+void WindowUpdate(Vector<string>& window, string word);
+bool PromptForWordsToGener(int& num, int N);
 
 
 int main()
@@ -28,9 +32,22 @@ int main()
     int N;
     while(!PromptForN(N));
 
-    Map<vector<string>, vector<string>> map;
+    Map<Vector<string>, Vector<string> > map;
+    ConstructMap(inFile, map, N);
 
+    Vector<Vector<string> > Keys = map.keys();
+    int map_key_length = Keys.size();
+    int index = randomInteger(1, map_key_length) - 1;
+    Vector<string> window = map[Keys[index]];
 
+    cout << endl;
+    int num;
+    while(!PromptForWordsToGener(num, N));
+    if (num == 0)
+    {
+        cout << "Exiting.";
+        return 0;
+    }
 
     return 0;
 }
@@ -80,14 +97,78 @@ bool PromptForN(int &num)
     return true;
 }
 
-void ConstructMap(ifstream& infile, Map<vector<string>, vector<string> > map)
+void ConstructMap(ifstream& infile, Map<Vector<string>, Vector<string> >& map, int N)
 {
     string word;
-    vector<string> window;
+    Vector<string> window;
+    Vector<string> save_head;
 
     while (infile >> word)
     {
-        if (window.size() < 2)
+        if (window.size() < N - 1)
+        {
             window.push_back(word);
+            save_head.push_back(word);
+            continue;
+        }
+
+        MapInsertKey(map, window, word);
+        WindowUpdate(window, word);
     }
+
+    for (int i = 0; i < N - 1; i++)
+    {
+        string word = save_head[i];
+        MapInsertKey(map, window, word);
+        WindowUpdate(window, word);
+    }
+}
+
+void MapInsertKey(Map<Vector<string>, Vector<string> >& map, Vector<string>& window, string word)
+{
+    if (map.containsKey(window))
+    {
+        map[window].push_back(word);
+    }
+
+    else
+    {
+        Vector<string> tmp;
+        tmp.push_back(word);
+        map[window] = tmp;
+    }
+}
+
+void WindowUpdate(Vector<string>& window, string word)
+{
+    window.remove(0);
+    window.push_back(word);
+}
+
+bool PromptForWordsToGener(int& num, int N)
+{
+    cout << "# of random words to generate (0 to quit)?";
+    string n_str;
+    getline(cin, n_str);
+
+    int i = 0;
+    for (char ch : n_str)
+    {
+        if (i == 0 && ch == '-')
+            continue;
+        if (!isdigit(ch))
+        {
+            cout << "Illegal interger format. Try again." << endl;
+            return false;
+        }
+    }
+
+    num = stringToInteger(n_str);
+    if (num < N && num != 0)
+    {
+        cout << "Must be at least " << N << " words." << endl << endl;
+        return false;
+    }
+
+    return true;
 }
