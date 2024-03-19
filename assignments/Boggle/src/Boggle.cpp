04296c5f02cb9cc39f94c87c834600309290a67e
Sigmal <2955106812@qq.com>
2024-03-19 20:52:11 +0800

    Learning CS106B: Day 18-02, read s4 doc

--- assignments/Boggle/src/Boggle.cpp
+++ assignments/Boggle/src/Boggle.cpp
@@ -1,9 +1,3 @@
-// This is a .cpp file you will edit and turn in.
-// We have provided a skeleton for you,
-// but you must finish it as described in the spec.
-// Also remove these comments here and add your own.
-// TODO: remove this comment header
-
 #include "Boggle.h"
 
 // letters on all 6 sides of every cube
