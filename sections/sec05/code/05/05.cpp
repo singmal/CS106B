45f9d7f28858b5a51448360a55ff183c70cc5ead
Sigmal <2955106812@qq.com>
2024-03-26 11:09:06 +0800

    Learning CS106B: Day 24, finished L22 and S5-Q1-4

---
+++ sections/sec05/code/05/05.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+struct ListNode
+{
+    int data;
+    ListNode *next;
+};
+
+bool listIsSortedRecursive(ListNode *list);
+bool listIsSortedNonrecursive(ListNode *list);
+
+int main()
+{
+    using std::cin;
+    using std::cout;
+    using std::endl;
+}
